b441279d454b6e57c8459b302ac47d0f89a1fad6
Brenner <43012551+Prelude14@users.noreply.github.com>
2019-04-04 14:41:28 -0700

    Update hello.py
    Intro is inside main game, along with changing buttons and mouse cursor change when over clickable objects.

--- hello.py
+++ hello.py
@@ -2,14 +2,10 @@ import pygame
 
 
 pygame.init()
-screen = pygame.display.set_mode((1400, 800))
 done = False
-is_blue = True
-#x = 30
-#y = 30
 
 #mouse cursor
-pygame.mouse.set_cursor((16, 19), (0, 0), (128, 0, 192, 0, 160, 0, 144, 0, 136, 0, 132, 0, 130, 0, 129, 0, 128, 128, 128, 64, 128, 32, 128, 16, 129, 240, 137, 0, 148, 128, 164, 128, 194, 64, 2, 64, 1, 128), (128, 0, 192, 0, 224, 0, 240, 0, 248, 0, 252, 0, 254, 0, 255, 0, 255, 128, 255, 192, 255, 224, 255, 240, 255, 240, 255, 0, 247, 128, 231, 128, 195, 192, 3, 192, 1, 128))
+pygame.mouse.set_cursor(*pygame.cursors.diamond)
 
 #colours
 red = (255,0,0)
@@ -27,47 +23,93 @@ GAME_FONT = pygame.font.SysFont(None, 30)
 
 clock = pygame.time.Clock()
 
+l = pygame.image.load("ers.png")
+s = pygame.image.load("start.png")
+ws = pygame.image.load("istart.png")
+b = pygame.image.load("bonus.png")
+ib = pygame.image.load("ibonus.png")
+
+
+class button(): 
+    """This class creates a invisible clickable box (or button)"""
+    def __init__(self, x, y, width, height):  
+        self.x = x 
+        self.y = y 
+        self.width = width 
+        self.height = height 
+        
+    def isOver(self, pos): #Pos is the mouse position [(x,y) coordinates] 
+        if self.x < pos[0] < self.x + self.width: 
+            if self.y < pos[1] < self.y + self.height: 
+                return True 
+        return False
+
+
+intro = True
+lvl1 = True
+
+while intro:
+#intro loop
+    gameDisplay = pygame.display.set_mode((500, 500))
+    pygame.display.set_caption('Welcome to ERS')#caption of display window
+                
+    gameDisplay.fill(white) #background
+    gameDisplay.blit(l,(55,0)) #ers
+    gameDisplay.blit(s,(95,175)) #start
+    start = button(141, 293, 205, 63) #creates button over top of start
+    gameDisplay.blit(b,(95,250)) #bonus
+    bonus = button(141, 355, 204, 67) #creates button over top of bonus
+
+    for event in pygame.event.get():
+        pos = pygame.mouse.get_pos()
+
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if start.isOver(pos):
+                intro = False
+            if bonus.isOver(pos):
+                intro = False
+            
+        if event.type == pygame.MOUSEMOTION:
+            if start.isOver(pos):
+                gameDisplay.blit(ws,(95,175)) #inverse start
+                pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+            elif start.isOver(pos) != True:
+                pygame.mouse.set_cursor(*pygame.cursors.arrow)  
+
+            if bonus.isOver(pos):
+                gameDisplay.blit(ib,(95,250)) #inverse bonus
+                pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+            elif bonus.isOver(pos) != True:
+                pygame.mouse.set_cursor(*pygame.cursors.arrow) 
+
+    pygame.display.flip()
+    clock.tick(60)
+
 press_dict = {'bed': {'mx_min': 850, 'mx_max': 1270, 'my_min': 570, 'my_max': 680,
                       'text': GAME_FONT.render("This bed reminds me of something, but I can't quite put my finger on it.", False, (255, 255, 255))},
                 'door': {'mx_min': 400, 'mx_max': 550, 'my_min': 310, 'my_max': 610,
                       'text': GAME_FONT.render("This door is locked.", False, (255, 255, 255))},
-                'key': {'mx_min': 610, 'mx_max': 640, 'my_min': 350, 'my_max': 410,
+                'key': {'mx_min': 600, 'mx_max': 640, 'my_min': 350, 'my_max': 410,
                       'text': GAME_FONT.render("FRREEEEEDDOOOM!!!!", False, (255, 255, 255))},
                 'ceiling': {'mx_min': 0, 'mx_max': 1400, 'my_min': 0, 'my_max': 115,
                       'text': GAME_FONT.render("That's weird, there are no lights in this room.", False, (255, 255, 255))}
 }
              
-
-# This text render queue will hold a dict with the text object to be displayed
-# as well as the number of game ticks it will be displayed
 text_render_queue = {}
 
 def button_press(mx,my,text_render_queue,press_dict=press_dict):
         """Function that checks coords of mouse click against dictionary
-        of known events and then adds them to the render queue
-        
-        modifies press_dict"""
+        of known events and then adds them to the render queue modifies press_dict"""
 
         for key in press_dict:
+                # This contains the actions and locations for all interaction related to clicking
                 if press_dict[key]['mx_min']<=mx<=press_dict[key]['mx_max'] and press_dict[key]['my_min']<=my<=press_dict[key]['my_max']:
                         #print('caught the button press {}'.format(key))
-                        text_render_queue[key] = {'text':press_dict[key]['text'],
-                                                  'ticks':50,
-                                                  # we could add more properties either constant
-                                                  # or specific to interaction
-                                                  }
+                        text_render_queue[key] = {'text':press_dict[key]['text'],'ticks':50,}
 
-# This contains the actions and locations for all interaction related to clicking
-# Will likely need better key names, or unique keys. This might also all be
-# better done using object, but I will let you decide.
-# press_dict contains dictionaires of the expected format:
-# {'mx_min': ,
-#  'mx_max': ,
-#  'my_min': ,
-#  'my_max': ,
-#  'text': ,
-#  #'ticks':200, # this could also be specified specifically for each interaction
-# }
 
 
 def render_text(text_render_queue):
@@ -75,24 +117,23 @@ def render_text(text_render_queue):
         for key in text_render_queue:
                 if text_render_queue[key]['ticks'] >= 0:
                         # render text to screen
-                        screen.blit(text_render_queue[key]['text'],
-                                    (110,10) )
+                        screen.blit(text_render_queue[key]['text'], (110,10))
                         # decrease the number of ticks left
                         text_render_queue[key]['ticks'] -= 1
-                        # This seems redundant, I suspect we can
-                        # move it outside the for-loop
                         pygame.display.update() 
                 else:
-                        # Remove that entry from the queue
-                        # text_render_queue.pop(key)
-                        # This doesn't actually work, Error:
-                        # RuntimeError: dictionary changed size during iteration
                         del_list.append(key)
-        ## cleanup
-        # Use list comprehension to loop fast
+        
+        # cleanup
         [text_render_queue.pop(key) for key in del_list]
+                        
 
-while not done:
+
+while lvl1: 
+#level1 game loop
+        screen = pygame.display.set_mode((1400, 800))
+        pygame.display.set_caption('ERS')#caption of display window
+
 
         screen.fill((0, 0, 0))
         """This is the background of the first room"""
@@ -103,11 +144,15 @@ while not done:
         pygame.draw.line(screen, green, (1400,0), (1190,114), 10)
         pygame.draw.line(screen, green, (0,800), (205,600), 10)
         pygame.draw.line(screen, green, (1400,800), (1190,600), 10)
+        #ceiling button
+        roof = button(0, 0, 1400, 115)
         #door
         pygame.draw.rect(screen, green, pygame.Rect(400, 310, 150, 300))
         pygame.draw.rect(screen, black, pygame.Rect(410, 320, 130, 280))
         pygame.draw.circle(screen, green, (520, 460), 10, 10)
         pygame.draw.circle(screen, black, (520, 460), 7, 7)
+        #door button
+        door = button(400, 310, 150, 290)
         #bed
         #solid colour
         pygame.draw.rect(screen, red, pygame.Rect(900, 640, 290, 40))
@@ -138,22 +183,39 @@ while not done:
         #blanket
         pygame.draw.line(screen, dred, (1110,570), (1190,640), 5)
         pygame.draw.line(screen, dred, (1190,640), (1190,682), 5)
+        #bed button
+        bed = button(840, 565, 400, 110)
 
-        #pygame.font.Font.render("Welcome to the first level", white)
+        #key sprite
+        k = pygame.image.load("key.png")
+        screen.blit(k,(590,350))
+        key = button(600, 350, 25, 50)
+        ik = pygame.image.load("bkey.png") #loads in missing key
 
         for event in pygame.event.get():
+                pos = pygame.mouse.get_pos()
                 if event.type == pygame.QUIT:
-                        done = True
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                        is_blue = not is_blue
+                        lvl1 = False
+                #if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                         mx, my = pygame.mouse.get_pos()
+                        #print (pygame.mouse.get_pos())
+                        # check mouse click against all registered coordinates
                         button_press(mx,my,text_render_queue)
-                        
-        
+                        if key.isOver(pos):
+                                screen.blit(ik,(590,350)) #inverse key
+                                pygame.display.update()
+                
+                if event.type == pygame.MOUSEMOTION:
+                        #if cursor is over any clickable objects changes cursor
+                        if key.isOver(pos) or bed.isOver(pos) or door.isOver(pos) or roof.isOver(pos):
+                                pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+                        elif key.isOver(pos) != True or bed.isOver(pos) != True or door.isOver(pos) != True or roof.isOver(pos) != True:
+                                pygame.mouse.set_cursor(*pygame.cursors.arrow)
+
+
         # render text in the queue
         render_text(text_render_queue)
-        print('text_render_queue:',text_render_queue)
         
         pygame.display.flip()
         clock.tick(60)
